2b2836d500258a4332f80890d93f32dccd7c6a70
Mark Wilkinson <markw@illuminae.com>
2020-11-05 16:55:31 +0100

    various stuff...

--- add_top_metadata.rb
+++ add_top_metadata.rb
@@ -10,66 +10,124 @@ fund = RDF::Vocabulary.new("http://vocab.ox.ac.uk/projectfunding#")
 skos =  RDF::Vocabulary.new("http://www.w3.org/2004/02/skos/core#")
 
 
+dctype =  RDF::Vocabulary.new("http://purl.org/dc/dcmitype/")
+foaf =  RDF::Vocabulary.new("http://xmlns.com/foaf/0.1/")
+locn	 =  RDF::Vocabulary.new("http://www.w3.org/ns/locn#")
+odrl	 =  RDF::Vocabulary.new("http://www.w3.org/ns/odrl/2/")
+owl	 =  RDF::Vocabulary.new("http://www.w3.org/2002/07/owl#")
+prov	 =  RDF::Vocabulary.new("http://www.w3.org/ns/prov#")
+
+rdfs	 =  RDF::Vocabulary.new("http://www.w3.org/2000/01/rdf-schema#")
+time	 =  RDF::Vocabulary.new("http://www.w3.org/2006/time#")
+vcard	 =  RDF::Vocabulary.new("http://www.w3.org/2006/vcard/ns#")
+xsd	 =  RDF::Vocabulary.new("http://www.w3.org/2001/XMLSchema#")
+
+
+
 client = LDP::LDPClient.new({
-	:endpoint => "http://training.fairdata.solutions/DAV/home/LDP/fair/grazing/285525/",
-	:username => "gofair",
-	:password => "gofair"})
+#	:endpoint => "http://ldp.cbgp.upm.es:8890/DAV/home/LDP/Training/grazing/285525/",
+#	:endpoint => "http://ejprd.fair-dtls.surf-hosted.nl:8890/DAV/home/LDP/EJP/",
+	:endpoint => "http://ejprd.fair-dtls.surf-hosted.nl:8890/DAV/home/LDP/EJP/erdri_catalog/",
+	:username => "ldp",
+	:password => "ldp"})
 distribution = client.toplevel_container
-
+puts distribution
 
   g = RDF::Graph.new() # a graph for the distribution
-	dist =  "http://training.fairdata.solutions/DAV/home/LDP/fair/grazing/285525/"
   
-  triplify(dist, dc.creator, "http://wilkinsonlab.info", g)
-  triplify(dist, dc.title, "A subset of the database of bio-ecological information on non-target arthropod species", g)
-  triplify(dist, dct.title, "A subset of the database of bio-ecological information on non-target arthropod species", g)
-	triplify(dist, dct.license, "http://data.europa.eu/euodp/kos/licence/EuropeanCommission", g)
-	triplify(dist, rdf.type, "http://purl.org/dc/dcmitype/Distribution", g)
-		
-	triplify(dist, dcat.accessURL, "http://training.fairdata.solutions/sparql", g)
-	triplify(dist, dcat.mediaType, "application/sparql-results+xml", g)
-
-  distribution.add_metadata(g.map {|s| [s.subject.to_s, s.predicate.to_s, s.object.to_s]}) 
+  dist =  "http://ejprd.fair-dtls.surf-hosted.nl:8890/DAV/home/LDP/EJP/erdri_catalog/"
+	
+  triplify(dist, dcat.themeTaxonomy, "http://ejprd.fair-dtls.surf-hosted.nl:8890/DAV/home/LDP/EJP/erdri_skos.rdf", g)
+  triples = g.map {|s| [s.subject.to_s, s.predicate.to_s, s.object.to_s]}
+  distribution.add_metadata(triples) 
+  abort
+  
+#	dist =  "http://ejprd.fair-dtls.surf-hosted.nl:8890/DAV/home/LDP/EJP/"
+#	
+#	[
+#	 'http://ejprd.fair-dtls.surf-hosted.nl:8890/DAV/home/LDP/EJP/EJP_HACK/' ,
+#	 'http://ejprd.fair-dtls.surf-hosted.nl:8890/DAV/home/LDP/EJP/erdri_catalog/',
+#	 'http://ejprd.fair-dtls.surf-hosted.nl:8890/DAV/home/LDP/EJP/orphanet_catalog/',
+#	 'http://ejprd.fair-dtls.surf-hosted.nl:8890/DAV/home/LDP/EJP/rd-sample_catalog/' ,
+#	 'http://ejprd.fair-dtls.surf-hosted.nl:8890/DAV/home/LDP/EJP/rdc-finder_catalog/' ,
+#	 'http://ejprd.fair-dtls.surf-hosted.nl:8890/DAV/home/LDP/EJP/bbmri-eric/',
+#	 'http://ejprd.fair-dtls.surf-hosted.nl:8890/DAV/home/LDP/EJP/rd-connect/'].each do |cat|
+#			triplify(dist, dcat.catalog, cat, g)
+#		end
+#  
+#  triplify(dist, foaf.homepage, "https://github.com/ejp-rd-vp/resource-metadata-schema", g)
+#  
+#  triplify(dist, dct.language, "http://id.loc.gov/vocabulary/iso639-1/en", g)
+#  triplify("http://id.loc.gov/vocabulary/iso639-1/en", rdf.type, dct.LinguisticSystem, g)
+#  triplify(dist, dcat.contactPoint, dist+"#contact", g)
+#  triplify(dist+"#contact", vcard.fn, "Mark Wilkinson", g)
+#  triplify(dist+"#contact", vcard.nickname, "MarkMoby", g)
+#  triplify(dist+"#contact", vcard.hasEmail, "mailto:markw@illuminae.com", g)
+#  
+#  triplify(dist, dct.creator, "https://github.com/simonjupp", g)
+#  triplify(dist, dct.creator, "https://github.com/S2Ola", g)
+#  triplify(dist, dct.creator, "https://github.com/rajaram5", g)
+#  triplify(dist, dct.creator, "https://github.com/ronaldcornet", g)
+#  triplify(dist, dct.creator, "https://github.com/markwilkinson", g)
+#  triplify("https://github.com/simonjupp", rdf.type, foaf.Person , g)
+#  triplify("https://github.com/S2Ola", rdf.type, foaf.Person , g)
+#  triplify("https://github.com/rajaram5", rdf.type, foaf.Person , g)
+#  triplify("https://github.com/ronaldcornet", rdf.type, foaf.Person , g)
+#  triplify("https://github.com/markwilkinson", rdf.type, foaf.Person , g)
+#  triplify(dist, dct.identifier, "http://ejprd.fair-dtls.surf-hosted.nl:8890/DAV/home/LDP/EJP/", g)
+#  triplify(dist, dct.publisher, "http://wilkinsonlab.info", g)
+#  triplify(dist, dct.title, "Top-level Catalogue for the EJP-RD hackathon", g)
+#  triplify(dist, dct.description, "An LDP Container to contain all of the (mock) catalogues for the EJP-RD project Hackathon", g)
+#	triplify(dist, dct.license, "https://creativecommons.org/share-your-work/public-domain/cc0/", g)
+#	triplify(dist, rdf.type, dcat.Catalog, g)
+#  triplify(dist, dcat.landingPage, "https://www.ejprarediseases.org/", g)		
+#
+#
+#  triples = g.map {|s| [s.subject.to_s, s.predicate.to_s, s.object.to_s]}
+#  puts triples
+  
+  #distribution.add_metadata() 
  
+  #g.each {|s| puts s.subject.to_s + "\t" + s.predicate.to_s + "\t" + s.object.to_s}
 
 
 
 
-client = LDP::LDPClient.new({
-	:endpoint => "http://training.fairdata.solutions/DAV/home/LDP/fair/grazing/",
-	:username => "gofair",
-	:password => "gofair"})
-dataset = client.toplevel_container
-
-
-  g = RDF::Graph.new() # a graph for the distribution
-	dset =  "http://training.fairdata.solutions/DAV/home/LDP/fair/grazing/"
-  
-  triplify(dset, dc.creator, "http://wilkinsonlab.info", g)
-  triplify(dset, dc.title, "A subset of the database of bio-ecological information on non-target arthropod species", g)
-  triplify(dset, dct.title, "A subset of the database of bio-ecological information on non-target arthropod species", g)
-  triplify(dset, dc.identifier, "10.5281/zenodo.285525", g)
-	triplify(dset, fund.hasPrincipalInvestigator,"Riedel, Judith", g)
-	triplify(dset, fund.hasPrincipalInvestigator,"Meissle, Michael", g)
-	triplify(dset, fund.hasPrincipalInvestigator,"Romeis, Jörg", g)
-	triplify(dset, rdf.type, "http://purl.org/dc/dcmitype/Dataset", g)
-	
-	triplify(dset, dcat.landingPage, "https://ring.ciard.net/update-and-expansion-database-bio-ecological-information-non-target-arthropod-species", g)
-	triplify(dset, dcat.landingPage, "https://zenodo.org/record/285525", g)
-	triplify(dset, dcat.landingPage, "http://training.fairdata.solutions/DAV/home/LDP/fair/grazing/", g)
-	
-	triplify(dset, dcat.distribution, dist, g)  #  LOOK!!!!
+#client = LDP::LDPClient.new({
+#	:endpoint => "http://ldp.cbgp.upm.es:8890/DAV/home/LDP/Training/grazing/",
+#	:username => "gofair",
+#	:password => "gofair"})
+#dataset = client.toplevel_container
+#
+#
+#  g = RDF::Graph.new() # a graph for the distribution
+#	dset =  "http://training.fairdata.solutions/DAV/home/LDP/fair/grazing/"
+#  
+#  triplify(dset, dc.creator, "http://wilkinsonlab.info", g)
+#  triplify(dset, dc.title, "A subset of the database of bio-ecological information on non-target arthropod species", g)
+#  triplify(dset, dct.title, "A subset of the database of bio-ecological information on non-target arthropod species", g)
+#  triplify(dset, dc.identifier, "10.5281/zenodo.285525", g)
+#	triplify(dset, fund.hasPrincipalInvestigator,"Riedel, Judith", g)
+#	triplify(dset, fund.hasPrincipalInvestigator,"Meissle, Michael", g)
+#	triplify(dset, fund.hasPrincipalInvestigator,"Romeis, Jörg", g)
+#	triplify(dset, rdf.type, dcat.Dataset, g)
+#	
+#	triplify(dset, dcat.landingPage, "https://ring.ciard.net/update-and-expansion-database-bio-ecological-information-non-target-arthropod-species", g)
+#	triplify(dset, dcat.landingPage, "https://zenodo.org/record/285525", g)
+#	triplify(dset, dcat.landingPage, "http://training.fairdata.solutions/DAV/home/LDP/fair/grazing/", g)
+#	
+#	triplify(dset, dcat.distribution, dist, g)  #  LOOK!!!!
 
 	# TO FILL IN ON THE DAY
 #	triplify(dset, dcat.theme, "ONTOLOGY TERM HERE", g)
-#	triplify("ONTOLOGY TERM HERE", skos.inScheme, "http://training.fairdata.solutions/DAV/home/LDP/fair/SKOS/pest_scheme.ttl", g)
+#	triplify("ONTOLOGY TERM HERE", skos.inScheme, "http://training.fairdata.solutions/DAV/home/LDP/fair/grazing/pest_skos.rdf", g)
 #	triplify(dset, dcat.theme, "ONTOLOGY TERM HERE", g)
-#	triplify("ONTOLOGY TERM HERE", skos.inScheme, "http://training.fairdata.solutions/DAV/home/LDP/fair/SKOS/pest_scheme.ttl", g)
+#	triplify("ONTOLOGY TERM HERE", skos.inScheme, "http://training.fairdata.solutions/DAV/home/LDP/fair/grazing/pest_skos.rdf", g)
 #	triplify(dset, dcat.theme, "ONTOLOGY TERM HERE", g)
-#	triplify("ONTOLOGY TERM HERE", skos.inScheme, "http://training.fairdata.solutions/DAV/home/LDP/fair/SKOS/pest_scheme.ttl", g)
+#	triplify("ONTOLOGY TERM HERE", skos.inScheme, "http://training.fairdata.solutions/DAV/home/LDP/fair/grazing/pest_skos.rdf", g)
 
 
-  dataset.add_metadata(g.map {|s| [s.subject.to_s, s.predicate.to_s, s.object.to_s]}) 
+#  dataset.add_metadata(g.map {|s| [s.subject.to_s, s.predicate.to_s, s.object.to_s]}) 
 
 
   #g.each {|s| puts s.subject.to_s + "\t" + s.predicate.to_s + "\t" + s.object.to_s}
---
+++ transform_data_new.rb
@@ -0,0 +1,109 @@
+require './utils.rb'
+require 'rdf'
+require 'ldp_simple'
+
+rdf =  RDF::Vocabulary.new("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+rdfs = RDF::Vocabulary.new("http://www.w3.org/2000/01/rdf-schema#")
+
+#ldp = RDF::Vocabulary.new("http://www.w3.org/ns/ldp#")
+
+sio = RDF::Vocabulary.new("http://semanticscience.org/resource/")
+uo =  RDF::Vocabulary.new("http://purl.obolibrary.org/obo/uo.owl#")
+efo = RDF::Vocabulary.new("http://www.ebi.ac.uk/efo/efo.owl#")
+geo = RDF::Vocabulary.new("http://www.w3.org/2003/01/geo/wgs84_pos#")
+
+lsid = RDF::Vocabulary.new("http://www.eu-nomen.eu/portal/taxon.php?GUID=")
+food = RDF::Vocabulary.new("http://data.food.gov.uk/codes/foodtype/hierarchy/facet/source/_")
+wiki = RDF::Vocabulary.new("https://en.wikipedia.org/wiki/ISO_3166-2:")
+
+
+client = LDP::LDPClient.new({
+	:endpoint => "http://training.fairdata.solutions/DAV/home/LDP/fair/grazing/",
+	:username => "gofair",
+	:password => "gofair"})
+top = client.toplevel_container
+
+puts "creating DCAT Distribution 285525"
+dcat_distribution = top.add_container(:slug => "285525")  # 285525 is the zenodo record id
+
+
+#http://w3id.org/FAIR_Training_LDP/grazing/
+#my =   RDF::Vocabulary.new("http://training.fairdata.solutions/DAV/home/LDP/fair/grazing/")
+my =   RDF::Vocabulary.new("http://w3id.org/FAIR_Training_LDP/grazing/285525/")
+
+obs = File.open("SpeciesAbundancePub2015.tsv")
+obs.readline # discard header
+spe = File.open("SpeciesInfoPub2015.tsv")
+spe.readline # discard header
+
+count = 0
+obs.each do |line|
+  (id, cropC, cropN, species, countryC, countryN, year, duration, long, lat, comments) = line.split("\t")
+  count += 1
+  break if count > 500
+  g = RDF::Graph.new()
+
+  observation = "obs_#{id}"    # obs_12345657
+  species = "species_#{species}"  # species_3456789
+  $stderr.puts "#{count} #{observation} #{species}"
+  triplify(my["#{observation}#obs"], rdf.type, sio.measuring, g)
+  triplify(my["#{observation}#obs"], rdfs.label, comments, g)
+  triplify(my["#{observation}#obs"], sio["is-located-in"], wiki[countryC], g)
+  triplify(my["#{observation}#obs"], sio["measured-at"], my["#{observation}#time"], g)
+  triplify(my["#{observation}#obs"], sio["has-participant"], my["#{observation}#infection"], g)
+  
+  triplify(my["#{observation}#location"], sio["is-location-of"], my["#{observation}#obs"], g)
+  triplify(my["#{observation}#location"], rdf.type, geo.Point, g)
+  triplify(my["#{observation}#location"], geo.lat, lat, g)
+  triplify(my["#{observation}#location"], geo.long, long, g)
+
+  triplify(wiki[countryC], rdf.type, sio.country, g)
+  triplify(wiki[countryC], rdfs.label, countryN, g)
+
+  triplify(my["#{observation}#time"], rdf.type, sio["time-interval"], g)
+  triplify(my["#{observation}#time"], sio["has-value"], duration, g)
+  triplify(my["#{observation}#time"], sio["has-unit"], uo["UO_0000036"], g)
+  triplify(uo["UO_0000036"], rdfs.label, "Year", g)
+  
+  triplify(my["#{observation}#infection"], rdf.type, efo["EFO_0001067"], g)
+  triplify(efo["EFO_0001067"], rdfs.label, "Parasitic Infection", g)
+  triplify(my["#{observation}#infection"], sio["has-participant"], my["#{species}#species"], g)
+  triplify(my["#{observation}#infection"], sio["has-participant"], food[cropC], g)
+  
+  triplify(food[cropC], rdfs.label, cropN, g)
+  triplify(food[cropC], rdf.type, sio.host, g)
+
+  triplify(my["#{species}#species"], rdf.type, sio.pathogen, g)
+
+  newresource = dcat_distribution.add_rdf_resource(:slug => observation)
+  newresource.add_metadata(g.map {|s| [s.subject.to_s, s.predicate.to_s, s.object.to_s]}) 
+ 
+end
+
+count = 0
+spe.each do |line|
+  (species, gbif, name) = line.split("\t")
+  next if gbif.empty? or name.empty? or species.empty?
+  species = "species_#{species}"  # species_3456789
+  count += 1
+  break if count > 500
+  $stderr.puts "#{count} #{species} SPECIES TABLE"
+
+  g = RDF::Graph.new()
+  triplify(my["#{species}#species"], rdf.type, sio.pathogen, g)
+  triplify(my["#{species}#species"], rdfs.label, name, g)
+  triplify(my["#{species}#species"], sio["has-identifier"], lsid[gbif], g)
+  triplify(my["#{species}#species"], rdf.type, sio.pathogen, g)
+  triplify(lsid["#{gbif}"], rdfs.label, name, g)
+  triplify(lsid["#{gbif}"], rdf.type, sio.identifier, g)
+
+  newresource = dcat_distribution.add_rdf_resource(:slug => species)
+  newresource.add_metadata(g.map {|s| [s.subject.to_s, s.predicate.to_s, s.object.to_s]}) 
+ 
+
+end
+
+
+
+
+
